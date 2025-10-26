b94f0e32c98caf89e66654347e598c6dc6114d6f
Shiki-12 <uknowndonp@gmail.com>
2025-10-26 22:49:30 +0700

    error dikit wkwkwk

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -478,7 +478,7 @@
         </div>
         <div class="col-lg-5">
           <div class="card h-100 p-3">
-            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('images/1.jpggi');">
+            <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('images/1.jpg');">
               <span class="mask bg-gradient-dark"></span>
               <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
                 <h5 class="text-white font-weight-bolder mb-4 pt-2">Work with me, Hotaru!</h5>
